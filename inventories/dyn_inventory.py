139dc1bc73b5c4519f12bfe0155ff6e7e364c412
Ryan Fitzpatrick <rfitzpat@redhat.com>
2017-06-21 15:43:21 -0400

    add ungrouped and vars to dyn_inventory.py

--- inventories/dyn_inventory.py
+++ inventories/dyn_inventory.py
@@ -14,8 +14,13 @@ inventory = {'group_one': {'hosts': ['group_one_host_0{}'.format(i) for i in ran
              'group_three': {'hosts': ['group_three_host_0{}'.format(i) for i in range(1, 6)]
                                     + ['group_two_and_three_host_0{}'.format(i) for i in range(1, 6)]
                                     + ['group_one_two_and_three_host_0{}'.format(i) for i in range(1, 6)],
-                             'vars': {'is_in_group_three': True}}}
-
+                             'vars': {'is_in_group_three': True}},
+             'all': {'vars': {'ansible_connection': 'local',
+                              'inventories_var': True}},
+             'ungrouped': {'hosts': ['ungrouped_host_0{}'.format(i) for i in range(1, 6)]},
+             '_meta': {'hostvars': {'group_one_host_01': {'group_one_host_01_has_this_var': True},
+                                    'group_two_host_01': {'group_two_host_01_has_this_var': True},
+                                    'group_three_host_01': {'group_three_host_01_has_this_var': True}}}}
 
 def parse_args():
     parser = ArgumentParser()
