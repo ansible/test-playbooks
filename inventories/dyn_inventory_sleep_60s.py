0e116bbd34dea0b67d4d6d6934a72efe38e09177
Alan Rominger <arominge@redhat.com>
2020-12-01 10:05:45 -0500

    Add 60 second sleep inventory script

---
+++ inventories/dyn_inventory_sleep_60s.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+import json
+
+import time
+
+inventory = {
+    'all': {'vars': {'ansible_connection': 'local'}},
+    'ungrouped': {'hosts': ['localhost']},
+    '_meta': {'hostvars': {'localhost': {
+        'current_time': str(datetime.now())
+    }}}
+}
+
+
+if __name__ == '__main__':
+    time.sleep(60)
+    print(json.dumps(inventory, indent=4))
