51471ed36d26a17a8e1a14c07c6c2eb03f9a301d
Dirk Jülich <djulich@redhat.com>
2025-04-29 17:47:21 +0200

    Update delete_var_from_src.py

--- inventories/delete_var_from_src.py
+++ inventories/delete_var_from_src.py
@@ -1,32 +1,57 @@
 #!/usr/bin/env python3
-"""
-Sometimes variable `x` is in the update, and sometimes it is not. This is
-usefule to test if a variable is deleted from the inventory if it has been
-deleted from the source between updates.
-"""
-
+# -*- coding: utf-8 -*-
 import json
 
-from random import random
+from datetime import datetime
+
+# This is a useful script to determine what happens on an inventory import
+# when the contents change. By using a timestamp, we should force the values
+# herein to change every time the sync is done.
+#
+# There are 2 examples
+#  - demonstrate new / old hosts by changing the hostname returned each time
+#         which is relevant to `overwrite`
+#  - demonstrate `overwrite_vars` by including 2 sub-examples
+#      - variable changes its name on every import
+#      - variable changes its value on every import
 
 
-delete_var: bool = random() < 0.5
+time_val = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S.%f')
 
-vars_dict = {"x_status": "deleted" if delete_var else "not deleted"}
-if not delete_var:
-    vars_dict["x"] = "testvalue"
+moover = "moover-{}".format(time_val)
 
-print(
-    json.dumps(
-        {
-            "_meta":
-            {
-                "hostvars": {},
+print(json.dumps({
+    "_meta": {
+        "hostvars": {
+            "change_of_vars": {
+                "static_key": "host_dynamic_{}".format(time_val),
+                "dynamic_{}".format(time_val): "host_static_value"
             },
-            "all":
-            {
-                "vars": vars_dict,
-            },
+            moover: {
+                "static_var": "static_value"
+            }
         }
-    )
-)
+    },
+    "all": {
+        "vars": {
+            "static_inventory_key": "inventory_dynamic_{}".format(time_val),
+            "dynamic_{}".format(time_val): "inventory_static_value"
+        }
+    },
+    "group_with_moover": {
+        "hosts": ["change_of_vars", moover]
+    },
+    "group_with_vars": {
+        "hosts": ["change_of_vars"],
+        "vars": {
+            "static_group_key": "group_dynamic_{}".format(time_val),
+            "dynamic_group_{}".format(time_val): "group_static_value"
+        }
+    },
+    "ungrouped": {
+        "hosts": [
+            moover,
+            "change_of_vars"
+        ]
+    }
+}))
